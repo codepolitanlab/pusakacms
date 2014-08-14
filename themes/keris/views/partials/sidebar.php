d0bdcdb04def390fb0eba29b2cbba734a37e2389
Toni Haryanto <toha.samba@gmail.com>
2014-08-14 16:53:50 +0700

    add sample page

--- themes/keris/views/partials/sidebar.php
+++ themes/keris/views/partials/sidebar.php
@@ -1,3 +1,3 @@
 <div class="sidebar">
-<?php echo $this->pusaka->generate_nav($this->uri->segment(1), 3, "nav nav-pills nav-stacked", "list", "active"); ?>
+<?php if($nav = $this->pusaka->generate_nav($this->uri->segment(1), 3, "nav nav-pills nav-stacked", "list", "active")) echo $nav; ?>
 </div>
