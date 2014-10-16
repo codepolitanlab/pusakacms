2bd4aa335fecf56e1f87edac95a21361af18834e
Toni Haryanto <toha.samba@gmail.com>
2014-10-16 17:45:46 +0700

    update style panel

---
+++ system/modules/panel/config/routes.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$route['panel/pages/new']	= "panel/new_page";
+$route['panel/posts/new']	= "panel/new_post";
--- system/modules/panel/views/dashboard.php
+++ system/modules/panel/views/dashboard.php
@@ -1,7 +1,6 @@
-<div class="panel panel-primary">
-  <div class="panel-body">
-	<a href="#" class="btn btn-lg btn-primary btn-tile"><span class="fa fa-pencil-square"></span><br>+ New post</a>
-	<a href="#" class="btn btn-lg btn-primary btn-tile"><span class="fa fa-file-text"></span><br>+ New page</a>
-	<a href="#" class="btn btn-lg btn-primary btn-tile"><span class="fa fa-file-image-o"></span><br>+ Upload Image</a>
-  </div>
+<div class="panel-dashboard">
+	<a href="{{ helpers.site_url }}panel/posts/new" class="btn btn-lg btn-primary btn-tile"><span class="fa fa-pencil-square"></span><br>+ New post</a>
+	<a href="{{ helpers.site_url }}panel/pages/new" class="btn btn-lg btn-info btn-tile"><span class="fa fa-file-text"></span><br>+ New page</a>
+	<a href="" class="btn btn-lg btn-success btn-tile"><span class="fa fa-file-image-o"></span><br>+ Upload Image</a>
+	<a href="#" class="btn btn-lg btn-warning btn-tile"><span class="fa fa-file-image-o"></span><br>+ Upload Image</a>
 </div>
