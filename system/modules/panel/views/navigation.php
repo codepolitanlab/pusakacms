797d5f6fb3a5e7336e66b80af700dc35ac0c62ce
Toni Haryanto <toha.samba@gmail.com>
2014-11-14 13:49:32 +0700

    change navigation list style

--- system/modules/panel/views/navigation.php
+++ system/modules/panel/views/navigation.php
@@ -22,19 +22,19 @@
 				</div>
 			</div>
 			<?php if($area_content): ?>
-				<table class="table">
+				<ul class="navlist">
 					<?php foreach ($area_content as $link): ?>
-						<tr>
-							<td width="30px"><span class="fa fa-align-justify" style="color:#aaa;line-height:27px;"></span></td>
-							<td width="20%"><?php echo $link['title']; ?></td>
-							<td><a href="<?php echo ($link['source'] == 'uri') ? site_url($link['url']) : $link['source'].$link['url']; ?>" target="_blank"><?php echo $link['url']; ?></a></td>
-							<td class="align-right">
+						<li>
+							<div><span class="fa fa-align-justify" style="color:#aaa;line-height:27px;"></span></div>
+							<div><?php echo $link['title']; ?></div>
+							<div><a href="<?php echo ($link['source'] == 'uri') ? site_url($link['url']) : $link['source'].$link['url']; ?>" target="_blank"><?php echo $link['url']; ?></a></div>
+							<div class="align-right pull-right">
 								<div class="option">
 									<a href="#" class="edit" data-mode="edit" data-toggle="modal" data-target="#linkModal" data-area="<?php echo $area_slug; ?>" data-title="<?php echo $link['title']; ?>" data-source="<?php echo $link['source']; ?>" data-url="<?php echo $link['url']; ?>" data-linktarget="<?php echo $link['target']; ?>"><span class="fa fa-edit"></span> Edit</a>
 									<a href="<?php echo site_url('panel/navigation/delete_link/'.$area_slug.'/'.urlencode($link['title'])); ?>" class="remove"><span class="fa fa-times"></span> Delete</a>
 								</div>
-							</td>
-						</tr>
+							</div>
+						</li>
 					<?php endforeach; ?>
 				</table>
 			<?php else: ?>
