df3ae6c1a3dae4a351a50e08bb1ee6fb6ea4104f
retsuyazhie <retsuya.zhie@gmail.com>
2014-11-15 16:43:41 +0700

    dashboard home with latest contact

--- system/modules/panel/views/dashboard.php
+++ system/modules/panel/views/dashboard.php
@@ -44,6 +44,15 @@
 					</div>
 				</div>
 			</div>
+
+			<div class="panel-home-contact">
+				<div class="panel-stat white">
+					<div class="stat-title">Contact</div>
+					<div class="stat-body">
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
