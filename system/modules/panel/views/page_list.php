ecfdbce3ee7f7ebd7a26e507db80ebf00f318551
Toni Haryanto <toha.samba@gmail.com>
2014-11-17 19:36:49 +0700

    update page list style for sortable plugin compatibility

--- system/modules/panel/views/page_list.php
+++ system/modules/panel/views/page_list.php
@@ -1,11 +1,11 @@
 <?php if(! empty($pages)):
 	foreach($pages as $uri => $content):
 		if($uri != '_title'): ?>
-    <li>
-        <div class="link">
-            <div class="dragpanel" title="klik and drag to rearrange"><span class="fa fa-align-justify" style="color:#aaa;line-height:27px;"></span></div>
-            <div><?php echo $content['_title']; ?></div>
-            <small><a href="<?php echo site_url($uri); ?>" target="_blank"><?php echo $uri; ?> <span class="fa fa-external-link"></span></a></small>
+    <li data-title="<?php echo $content['_title']; ?>" data-url="<?php echo $uri; ?>" class="dd-item dd3-item" id="page">
+        <div class="dd-handle dd3-handle" title="klik and drag to rearrange"><span class="fa fa-align-justify" style="color:#aaa;line-height:27px;"></span></div>
+        <div class="dd3-content">
+            <?php echo $content['_title']; ?>
+            <small><a href="<?php echo site_url($uri); ?>" target="_blank"><?php echo $uri; ?></a></small>
             <div class="align-right pull-right">
                 <div class="option">
                     <a href="<?php echo site_url('panel/pages/edit?page='.$uri); ?>" class="edit" title="Edit"><span class="fa fa-edit"></span></a>
@@ -14,10 +14,10 @@
                 </div>
             </div>
         </div>
-        <ul class="navlist">
-            <?php if(count($content) > 1): ?>
+        <?php if(count($content) > 1): ?>
+            <ol class="dd-list">
 				<?php echo Modules::run('panel/pages/_page_list', $content); ?>
+            </ol>
 		<?php endif; ?>
-        </ul>
     </li>
 <?php endif; endforeach; endif; ?>
--- system/modules/panel/views/pages.php
+++ system/modules/panel/views/pages.php
@@ -12,10 +12,32 @@
 
 <div class="panel panel-default">
 	<div class="panel-body">
-		<div class="draggable dd">
-			<ul class="navlist dd-list">
+		<div class="dd">
+            <ol class="dd-list">
 				<?php echo $pages; ?>
-			</ul>
+			</ol>
 		</div>
 	</div>
 </div>
+
+<script>
+    $(function(){
+	    $('.dd').nestable({group: 1}).on('change', updateSort);
+    })
+
+    // callback to update arrange navigation
+    var updateSort = function(e)
+    {
+        var list   = e.length ? e : $(e.target);
+        var area = list.attr('id');
+        if (window.JSON) {
+            var newmap = window.JSON.stringify(list.nestable('serialize'));
+            $.post(BASE_URL+'panel/pages/sort/'+area, {newmap : newmap})
+            .done(function(data){
+                console.log(data);
+            });
+        } else {
+            output.val('JSON browser support required for this demo.');
+        }
+    };
+</script>
