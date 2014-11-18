e305793c59b22c8aeeac38663c955709e9a18547
retsuyazhie <retsuya.zhie@gmail.com>
2014-11-18 07:23:54 +0700

    blog home link

--- system/themes/keris/views/partials/header_blog.php
+++ system/themes/keris/views/partials/header_blog.php
@@ -1,6 +1,6 @@
 <div class="pusaka-blog-header text-center">
 	<div class="container">	
-		{{ func.get_theme_image file="kujang_logo_sm.png" }}
+		<a href="<?php echo site_url(); ?>blog/">{{ func.get_theme_image file="kujang_logo_sm.png" }}</a>
 		<h1>PusakaCMS Blog</h1>
 		<div>Informasi, diskusi dan sharing seputar PusakaCMS</div>
 	</div>
