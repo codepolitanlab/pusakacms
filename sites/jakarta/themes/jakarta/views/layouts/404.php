b4b18c1cbf6e8dceee7f165346f77afd4c9ab6c9
retsuyazhie <retsuya.zhie@gmail.com>
2014-11-20 16:05:50 +0700

    jakarta 404 layout

---
+++ sites/jakarta/themes/jakarta/views/layouts/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php get_partial('metadata'); ?>
+</head>
+<body class="notfound-wrapper">
+
+	<div class="notfound-inner text-center">
+
+		<div class="container">		
+			
+			<h1>Error 404</h1>
+					
+			<div class="text-wrapper">Page not found, guys..</div>
+
+			<a href="<?php echo site_url(); ?>" class="btn btn-primary btn-lg"><span class="fa fa-long-arrow-left"></span> Back to Jakarta</a>
+				
+		</div>
+
+	</div>
+	
+</body>
+</html>
