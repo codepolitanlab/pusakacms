10b4ab7690dde5815450c362dd1b1b74dff0a5e7
retsuyazhie <retsuya.zhie@gmail.com>
2014-11-21 16:51:12 +0700

    orange style

--- sites/jakarta/themes/jakarta/views/partials/metadata.php
+++ sites/jakarta/themes/jakarta/views/partials/metadata.php
@@ -23,4 +23,4 @@
 
 <!-- Custom styles for this template -->
 <link href="<?php echo get_theme_url() ?>assets/css/main_style.css" rel="stylesheet">
-<link href="<?php echo get_theme_url() ?>assets/css/color-hijau.css" rel="stylesheet">
+<link href="<?php echo get_theme_url() ?>assets/css/color-orange.css" rel="stylesheet">
