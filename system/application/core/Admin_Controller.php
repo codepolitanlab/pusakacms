80949e825555595b3c240092b0192b496aadf209
Toni Haryanto <toha.samba@gmail.com>
2014-11-26 14:47:07 +0700

    add caution for unchanged default admin password

--- system/application/core/Admin_Controller.php
+++ system/application/core/Admin_Controller.php
@@ -8,6 +8,12 @@ class Admin_Controller extends MY_Controller
 
 		// set theme
 		$this->template->set_theme($this->config->item('admin_theme'));
+
+		if(file_exists('sites/'.SITE_SLUG.'/users/admin.json')){
+			$admin = json_decode(file_get_contents('sites/'.SITE_SLUG.'/users/admin.json'), true);
+			if(isset($admin['password']) && $admin['password'] == 'password')
+				$this->template->set('warning', 'Change your default admin password to secure one in <a href="'.site_url('panel/users/edit/admin').'"><strong>Users settings</strong></a> page first.');
+		}
 	}
 
 }
